8e9b8281f43dab0b7121436b7930bb50d7981e83
Augusto Cardoso <acardoso.ti@gmail.com>
2024-09-18 22:27:54 -0300

    Update errors.go

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	ErrInvalidProtocol = errors.New("invalid protocol, only http or https are allowed")
 	ErrInvalidHost     = errors.New("invalid host")
-	ErrInvalidPort     = errors.New("invalid port; Network ports in TCP and UDP range from number zero up to 65535")
+	ErrInvalidPort     = errors.New("invalid port; Network ports in TCP and UDP range from 0 to 65535")
 	ErrInvalidPath     = errors.New("invalid path")
 )
